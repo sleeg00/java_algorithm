6dbc79a8729160413bcd2cba0ebd22cff8e6b150
sleeg00 <slee000220@gmail.com>
2024-12-08 16:41:00 +0900

    오답

--- Boj_17609/Main.java
+++ Boj_17609/Main.java
@@ -2,65 +2,58 @@ package org.example.Boj_17609;
 
 import java.util.*;
 
-// 정의
-// 회문 or 펠린드롬은 <-,-> 방향으로 같은 순서의 문자로 구성된 문자열이다.
-// ex) "abba", "kayak", "reviver"...
-// 문자열이 회문이 아니지만 한 문자를 삭제해 회문으로 만들수 있는지?
-// 회문이면 0, 만들 수 있는 회문이면 1, 그 외 2 출력
-//-------------------------------------------------/
-// 조건
-// 1 <= 문자 개수(T) <= 30, 3 <= 문자 길이 <= 100,000 (알파벳 소문자)
-//-------------------------------------------------/
-// 해결 방안
-// 1. Tow Pointer
-// 2. 0번 Index, length()-1 Index = x, y
-// 3. x == y -> x+1, y-1, else x+1==y x+1, x+=2, y-1, else x==y-1 => x+1, y-2 =>sw=1
-// 4. sw==2이면 break 2, sw==0 print(0) else print(1)
-// ex) aaabbbaaac
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int x, y;
-        int T = sc.nextInt();
+        int N = sc.nextInt();
         sc.nextLine();
-        for (int i = 0; i < T; i++) {
-            String str = sc.nextLine();
-            x = 0;
-            y = str.length() - 1;
-            int sw=0;
-            while (x < y) {
 
-                if (str.charAt(x) == str.charAt(y)) {
-                    x++; y--;
-                } else if (str.charAt(x + 1) == str.charAt(y)) {
-                    x += 2; y--;
-                    sw++;
-                } else if (str.charAt(x) == str.charAt(y - 1)) {
-                    x++; y -= 2;
-                    sw++;
-                } else {
-                    sw=2;
-                    break;
-                }
-            }
-            x=0;
-            y=str.length()-1;
+        for (int i = 0; i < N; i++) {
+            String str = sc.nextLine();
+            int front = 0;
+            int back = str.length() - 1;
+            int sw = 0;
             int im = 0;
-            while (x < y) {
-                if (str.charAt(x) == str.charAt(y)) {
-                    x++; y--;
-                }  else if (str.charAt(x) == str.charAt(y - 1)) {
-                    x++; y -= 2;
-                    im++;
-                } else if (str.charAt(x + 1) == str.charAt(y)) {
-                    x += 2; y--;
-                    im++;
+            while (true) {
+                if (front >= back) {
+                    break;
+                }
+                if (str.charAt(front) == str.charAt(back)) {
+                    front++;
+                    back--;
+                } else if (str.charAt(front + 1) == str.charAt(back) && sw == 0) {
+                    front++;
+                    sw = 1;
+                } else if (str.charAt(front) == str.charAt(back - 1) && sw == 0) {
+                    back--;
+                    sw = 1;
                 } else {
-                    im=2;
+                    sw = 2;
                     break;
                 }
             }
-            System.out.println(Math.min(sw,im));
+            front = 0;
+            back = str.length() - 1;
+            while (true) {
+                if (front >= back) {
+                    break;
+                }
+                if (str.charAt(front) == str.charAt(back)) {
+                    front++;
+                    back--;
+                } else if (str.charAt(front) == str.charAt(back - 1) && im == 0) {
+                    back--;
+                    im = 1;
+                } else if (str.charAt(front + 1) == str.charAt(back) && im == 0) {
+                    front++;
+                    im = 1;
+                } else {
+                    im = 2;
+                    break;
+                }
+            }
+            System.out.println(Math.min(im, sw));
         }
     }
 }
--- Boj_9996/Main.java
+++ Boj_9996/Main.java
@@ -2,98 +2,68 @@ package org.example.Boj_9996;
 
 import java.util.Scanner;
 
-// 문제 정의
-// 패턴 = 알파벳 소문자, 별포 1개 로 이루어짐
-// 패턴에 있는 '*'을 알파벳 소문자로 이루어진 문자열로 변환해 파일 이름과 같게 만들수 있어야 한다.
-//-----------------------------
-// (조건)
-// 별포는 ' ' (빈 문자열)로 바꿀 수도 있다.
-// ex) "abcd", "ad", "anestonestod"는 패턴 a*d와 일치, "bcd"는 일치하지 않음
-// 1 <= 파일 개수(N) <= 100
-// 패턴 = 알파벳 소문자와 별표 1개 (알파벳은 여러개 가능)
-// 문자열 길이 <= 100
-//---------------------------
-// 예외 생각
-// a*d 가능 , *ad 가능 ad* 가능, aa*d, a*bbbbbd 가능 => 마지막과 앞에 '*'가 오면 하나만 체크 중간이면 끝과 시작 체크
-// 공백 생각해야함
-// 해결 방안
-// 1. 중간에 '*'가 있을 경우 : Pattern의 앞, 뒤 문자열과 str의 앞, 뒤가 같은지 Check
-// 2. 앞에 '*'가 있을 경우 : Pattern의 뒤 문자열, str의 뒤 문자열 같은지 check
-// 3. 뒤에 '*'가 있을 경우 : Pattern의 앞 문자열, str의 앞 문자열 같은지 check
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = Integer.parseInt(sc.nextLine());
+
+        int N = sc.nextInt();
+        sc.nextLine();
         String pattern = sc.nextLine();
+        int front = 0, back = 0;
         int sw = 0;
-        int x = 0, y = 0;
         if (pattern.charAt(0) == '*') {
             sw = 0;
         } else if (pattern.charAt(pattern.length() - 1) == '*') {
             sw = 1;
         } else {
-            sw = 2;
             for (int i = 0; i < pattern.length(); i++) {
+                sw = 2;
                 if (pattern.charAt(i) == '*') {
-                    x = i - 1;
-                    y = pattern.length() - 1 - (i + 1);
+                    front = i - 1;
+                    back = i + 1;
                     break;
                 }
             }
         }
-
         for (int i = 0; i < N; i++) {
             String str = sc.nextLine();
+            boolean ans = true;
             if (sw == 0) {
-                int index = pattern.length() - 1;
-                while (true) {
-                    if (str.charAt(index) != pattern.charAt(index)) {
-                        System.out.println("NE");
-                        break;
-                    }
-                    index--;
-                    if (index == 0) {
-                        System.out.println("DA");
+                int index = str.length() - 1;
+                for (int j = pattern.length() - 1; j >= 1; j--) {
+                    if (str.charAt(index--) != pattern.charAt(j)) {
+                        ans = false;
                         break;
                     }
                 }
             } else if (sw == 1) {
-                int index = 0;
-                while (true) {
-                    if (str.charAt(index) != pattern.charAt(index)) {
-                        System.out.println("NE");
-                        break;
-                    }
-                    index++;
-                    if (index == pattern.length() - 1) {
-                        System.out.println("DA");
-                        break;
-                    }
-                }
-            } else {
-                int check = 0;
-                for (int j = 0; j <= x; j++) {
-
+                for (int j = 0; j < pattern.length() - 1; j++) {
                     if (str.charAt(j) != pattern.charAt(j)) {
-                        check = 1;
+                        ans = false;
                         break;
                     }
                 }
-                for (int j = 0; j <= y; j++) {
-                    if (str.charAt(str.length() - 1 - j) != pattern.charAt(pattern.length() - 1 - j)) {
-                        check = 1;
-                        break;
-                    }
-                    if(str.length()-1-j <= x) {
-                        check = 1;
+            } else if (sw == 2) {
+                for (int j = 0; j <= front; j++) {
+                    if (str.charAt(j) != pattern.charAt(j)) {
+                        ans = false;
                         break;
                     }
                 }
-                if (check == 1) {
-                    System.out.println("NE");
-                } else {
-                    System.out.println("DA");
+                int index = str.length() - 1;
+                for (int j = pattern.length() - 1; j >= back; j--) {
+                    if (str.charAt(index) != pattern.charAt(j)  ||index <= front) {
+                        ans = false;
+                        break;
+                    }
+                    index--;
                 }
+            }
+            if (ans == false) {
+                System.out.println("NE");
+            } else {
+                System.out.println("DA");
             }
         }
     }
