8c10f697b21b480a21f942871d728a33448d2990
sleeg00 <slee000220@gmail.com>
2025-05-07 22:01:28 +0900

    오답

--- Boj_2294/Main.java
+++ Boj_2294/Main.java
@@ -2,8 +2,9 @@ package org.example.Boj_2294;
 
 import java.util.*;
 import java.io.*;
+
 public class Main {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
@@ -11,32 +12,30 @@ public class Main {
         st = new StringTokenizer(br.readLine(), " ");
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
-        int[] coin = new int[N+1];
-        int[] Dp = new int[K+1];
-
-        for(int i=1; i<=N; i++) {
-            coin[i] = Integer.parseInt(br.readLine());
-        }
-        Arrays.sort(coin);
-        for(int i=1; i<=K; i++) {
+        int[] coin = new int[N + 1];
+        int[] Dp = new int[K + 1];
+        for (int i = 0; i <= K; i++) {
             Dp[i] = Integer.MAX_VALUE;
         }
-        Dp[0]=0;
-        for(int i=1; i<=N; i++) {
-            int cost = coin[i];
-            for(int j=cost; j<=K; j++) {
-                if(Dp[j-cost]==Integer.MAX_VALUE) {
-                    Dp[j] = Math.min(Dp[j], Dp[j-cost]);
-                } else {
-                    Dp[j] = Math.min(Dp[j], Dp[j - cost] + 1);
+        for (int i = 0; i < N; i++) {
+            coin[i] = Integer.parseInt(br.readLine());
+        }
+        Dp[0] = 0;
+
+        for (int i = 0; i <= K; i++) {
+            for (int j = 0; j < N; j++) {
+                int cost = coin[j];
+                if (i + cost <= K && Dp[i]!=Integer.MAX_VALUE) {
+                    Dp[i + cost] = Math.min(Dp[i + cost], Dp[i] + 1);
                 }
             }
+
         }
 
-        if(Dp[K]!=Integer.MAX_VALUE) bw.write(Dp[K] + " ");
-        else bw.write(-1 + " ");
-        bw.flush();
-        bw.close();
-        br.close();
+        if (Dp[K] != Integer.MAX_VALUE) {
+            System.out.println(Dp[K]);
+        } else {
+            System.out.println(-1);
+        }
     }
 }
