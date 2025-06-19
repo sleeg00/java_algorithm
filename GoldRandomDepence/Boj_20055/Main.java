ce51132099965a8855e59f708743cbb479a72bfd
sleeg00 <slee000220@gmail.com>
2025-06-19 14:28:04 +0900

    20055

--- GoldRandomDepence/Boj_20055/Main.java
+++ GoldRandomDepence/Boj_20055/Main.java
@@ -15,49 +15,81 @@ import java.io.*;
 // 4 : 내구도가 0인 칸의 개수가 K개 이상이라면 종료 (while) 아니라면 1로 이동
 // ans : 종료시 몇 번째 단계가 진행 중이었는지?
 public class Main {
-    public static class Node {
-        int health;
-        boolean robot;
-
-        Node(int health, boolean robot) {
-            this.health = health;
-            this.robot = robot;
-        }
-    }
-
-    static int step = 1;
-    static int lengthOfBelt, K;
-    static ArrayDeque<Node> deque = new ArrayDeque<Node>();
+    static int n, k;
+    static int[] A;
+    static boolean[] robot;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        lengthOfBelt = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
-        st = new StringTokenizer(br.readLine(), " ");
-        for (int i = 0; i < lengthOfBelt; i++) {
-            deque.addLast(new Node(Integer.parseInt(st.nextToken()), false));
+
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+
+        A = new int[2 * n];
+        robot = new boolean[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < A.length; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
         }
 
-        while (true) {
-            forwardOfBelt();// 1번
-            // 2번
-            healthZeroCountEqualsK();// 4번
+        System.out.println(simulation());
 
-            // 3번
-            healthZeroCountEqualsK();// 4번
-            step++;
+    }
+
+    private static int simulation() {
+        int cnt = 0;
+
+        while (isOk()) {
+            // 1. 벨트가 각 칸 위에 있는 로봇과 함께 한 칸 회전
+            int tmp = A[A.length - 1];
+            for (int i = A.length - 1; i > 0; i--) {
+                A[i] = A[i - 1];
+            }
+            A[0] = tmp;
+
+            for (int i = robot.length - 1; i > 0; i--) {
+                robot[i] = robot[i - 1];
+            }
+            robot[0] = false;
+            robot[n - 1] = false;
+
+            // 2. 가장 먼저 벨트에 올라간 로봇부터 벨트가 회전하는 방향으로 한칸 이동 가능
+            // 로봇 이동위해 이동하려는 칸에 로봇없으며 그 칸의 내구도가 1이상 남아있어야함
+            for (int i = robot.length - 1; i > 0; i--) {
+                if (robot[i - 1] && !robot[i] && A[i] >= 1) {
+                    A[i]--;
+                    robot[i] = true;
+                    robot[i - 1] = false;
+                }
+            }
+
+            // 3. 올리는 위치에 있는 칸의 내구도가 0이 아니면 올리는 위치에 로봇 올림
+            if (A[0] > 0) {
+                robot[0] = true;
+                A[0]--;
+            }
+
+            cnt++;
         }
-        System.out.print(step);
+
+        return cnt;
     }
 
-    private static void forwardOfBelt() {
-        Node endPoint = deque.pollLast();
-        deque.addFirst(endPoint);
-        getOffEndPointRobot(endPoint);
+    private static boolean isOk() {
+        // 4. 내구도가 0 칸의 개수가 k개 이상이라면 과정을 종료 , 그렇지 않다면 1번으로 돌아감
+        int cnt = 0;
+        for (int i = 0; i < A.length; i++) {
+            if (A[i] == 0) {
+                cnt++;
+            }
+            if (cnt >= k)
+                return false;
+        }
+        return true;
     }
 
-    private static void getOffEndPointRobot(Node endPoint) {
-        if(endPoint.robot) endPoint.robot = false;
-    }
 }
+
